155c1a5e389a6575fb57d70568a8de8bb988c2a3
zahraMajed <zahra_m.alzawad@hotmail.com>
2022-10-27 16:48:51 +0300

    Test Intro View

--- InnerPeace/View/Test Views/TestIntroView.swift
+++ InnerPeace/View/Test Views/TestIntroView.swift
@@ -14,11 +14,82 @@ struct TestIntroView: View {
     
     //MARK: Body
     var body: some View {
-        Text("Hello, test Homepage is here!")
-            .fullScreenCover(isPresented: $shouldShowOnboarding) {
-                OnboardingScreensView(shouldShowOnboarding: $shouldShowOnboarding)
-        }
-    }
+        VStack {
+            Image("OnBording2")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 390, height: 238)
+                .clipped()
+                .offset(x: 0, y: 100)
+            
+            //Title and Subtitle
+            VStack (alignment: .center,spacing: 18) {
+                Text("Fisrt, we’re going to go through a few question")
+                    .font(.custom("Helvetica", size: 25))
+                    .bold()
+                    .textCase(.uppercase)
+                Text("To help you build new habits in a wellness routine! Here is our journey")
+                    .fontWeight(.light)
+                    .font(.custom("Helvetica", size: 18))
+                    .frame(width: 362, height: 66, alignment: .center)
+            }.offset(x: 0, y: 150)
+            
+            //Creating the journey path
+            ZStack {
+                Path() { path in
+                    path.move(to: CGPoint(x: 0, y: 145))
+                    path.addLine(to: CGPoint(x: 390, y: 145))
+                }
+                .stroke(Color("SecondaryColorGreyGreen"), lineWidth: 2)
+
+                VStack (spacing: 10){
+                    ZStack {
+                        HStack(spacing: 100){
+                            Circle()
+                                .frame(width: 32, height: 32)
+                            Circle()
+                                .frame(width: 32, height: 32)
+                            Circle()
+                                .frame(width: 32, height: 32)
+                        }.foregroundColor(Color("SecondaryColorGreyGreen"))
+                        HStack(spacing: 121){
+                            Text("1")
+                                .font(.custom("Helvetica", size: 21))
+                            Text("2")
+                                .font(.custom("Helvetica", size: 21))
+                            Text("3")
+                                .font(.custom("Helvetica", size: 21))
+                        }
+                        .foregroundColor(.white)
+                    }
+                    HStack(spacing: 40){
+                        Text("Anxiety test")
+                            .font(.custom("Helvetica", size: 12))
+                            .fontWeight(.light)
+                            .padding(.leading, 20.0)
+                        Text("Depression test")
+                            .font(.custom("Helvetica", size: 12))
+                            .fontWeight(.light)
+                            .padding(.leading)
+                        Text("Habits prefrences")
+                            .font(.custom("Helvetica", size: 12))
+                            .fontWeight(.light)
+                            .padding(.leading, 10.0)
+                    }
+                }
+            }.offset(x: 0, y: 70)
+            
+            //Button
+            Button("Start Anxiety test") {
+                }
+            .frame(width: 327, height: 41)
+            .background(Color("PrimaryColorB"))
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            Spacer()
+        }.fullScreenCover(isPresented: $shouldShowOnboarding) {
+            OnboardingScreensView(shouldShowOnboarding: $shouldShowOnboarding)}
+    }//end body
 }
 
 struct TestIntroView_Previews: PreviewProvider {
