fefb0d1c0d2bc3a82733a20f1292b97535028832
zahraMajed <zahra_m.alzawad@hotmail.com>
2022-10-27 09:32:59 +0300

    Initial Commit

---
+++ InnerPeace/InnerPeaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnerPeaceApp.swift
+//  InnerPeace
+//
+//  Created by Zahra Majed Alzawad on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct InnerPeaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
