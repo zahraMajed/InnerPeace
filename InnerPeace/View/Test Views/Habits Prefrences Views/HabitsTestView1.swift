536dadcb2a22512fb69e248eb4d7115839a3294b
zahraMajed <zahra_m.alzawad@hotmail.com>
2022-10-31 17:04:55 +0300

    ..

---
+++ InnerPeace/View/Test Views/Habits Prefrences Views/HabitsTestView1.swift
@@ -0,0 +1,20 @@
+//
+//  HabitsTestView1.swift
+//  InnerPeace
+//
+//  Created by Zahra Majed Alzawad on 06/04/1444 AH.
+//
+
+import SwiftUI
+
+struct HabitsTestView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HabitsTestView1_Previews: PreviewProvider {
+    static var previews: some View {
+        HabitsTestView1()
+    }
+}
