de6be0b8ff47d1cc28f14279b6e75253b7792dff
zahraMajed <zahra_m.alzawad@hotmail.com>
2022-11-01 14:12:42 +0300

    Anxiety test

---
+++ InnerPeace/View/Test Views/Anxiety Test Views/AnxietyTestView1.swift
@@ -0,0 +1,20 @@
+//
+//  AnxietyTestView1.swift
+//  InnerPeace
+//
+//  Created by Zahra Majed Alzawad on 07/04/1444 AH.
+//
+
+import SwiftUI
+
+struct AnxietyTestView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnxietyTestView1_Previews: PreviewProvider {
+    static var previews: some View {
+        AnxietyTestView1()
+    }
+}
