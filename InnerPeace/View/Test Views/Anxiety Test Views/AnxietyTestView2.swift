8775ca5c131759a9ddda6678103ea481ff5049d3
zahraMajed <zahra_m.alzawad@hotmail.com>
2022-11-02 09:57:19 +0300

    ..

--- InnerPeace/View/Test Views/Anxiety Test Views/AnxietyTestView2.swift
+++ InnerPeace/View/Test Views/Anxiety Test Views/AnxietyTestView2.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct AnxietyTestView2: View {
+    @EnvironmentObject var anxietySettingObj : AnxietySettings
+    
+    let questionDic: [Int: String] = [0:"Not at all sure",
+                                      1:"Several days",
+                                      2:"Over half the days",
+                                      3:"Nearly every day"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+        Spacer()
+            Text("Anxiety Test")
+            .font(.custom("Helvetica", size: 34))
+            .fontWeight(.bold)
+            .foregroundColor(Color.black)
+            .frame(width: 353, height: 60, alignment: .leading)
+
+//Questions
+            Text("Feeling nervous, anxious, or on edge")
+            .font(.custom("Helvetica", size: 22))
+            .fontWeight(.bold)
+            .frame(width: 361, height: 52.0, alignment: .leading)
+            .offset(x:10,y: 80)
+        
+// Answers
+            VStack(spacing: 20){
+            ForEach(questionDic.sorted(by: >), id: \.key) {
+                key, value in
+                NavigationLink {
+                    AnxietyTestView3()
+                } label: {
+                    Text("\(value)")
+                        .font(.custom("Helvetica", size: 21))
+                        .foregroundColor(Color.black)
+                        .frame(width: 346.0, height: 48.0)
+                        .background(Color.white)
+                        .cornerRadius(8)
+                        .shadow(color: Color("PrimaryColorB"), radius: 2, x: 0, y: 2)
+                }.simultaneousGesture(TapGesture().onEnded{
+                    anxietySettingObj.score += key
+                    print("The updated score in view 2 : \(anxietySettingObj.score)")
+                })
+            }//foreach
+        }.padding().offset(x: 0, y:130)
+
+//ProgressPar
+            ZStack {
+                Text("1/7")
+                    .font(.custom("Helvetica", size: 21))
+                    .fontWeight(.regular)
+                    .multilineTextAlignment(.leading)
+                    .frame(width: 361, height: 52.0, alignment: .leading)
+                    .offset(x:2,y: 100)
+                Path() { path in
+                    path.move(to: CGPoint(x: 20, y: 300))
+                    path.addLine(to: CGPoint(x: 370, y: 300))
+                }.stroke(Color("Gray"), lineWidth: 4)
+                
+                Path() { path in
+                    path.move(to: CGPoint(x: 20, y: 300))
+                    path.addLine(to: CGPoint(x: 140, y: 300))
+                }.stroke( Color("SecondaryColorGreyGreen"), lineWidth: 4)
+            }
+        }//genral VStack
     }
 }
 
---
+++ InnerPeace/View/Test Views/Anxiety Test Views/AnxietyTestView3.swift
@@ -0,0 +1,20 @@
+//
+//  AnxietyTestView3.swift
+//  InnerPeace
+//
+//  Created by Zahra Majed Alzawad on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+struct AnxietyTestView3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnxietyTestView3_Previews: PreviewProvider {
+    static var previews: some View {
+        AnxietyTestView3()
+    }
+}
